4cf6c1218fdd7563b682de371f826dd9ca6d2d0e
javohir12312 <abduxalilovjavohir393@gamil.com>
2023-05-03 17:35:36 +0500

    correct reponsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,53 +6,52 @@ import Katalog from "../Header/Katalog/Katalog"
 const Home = () => {
   return (
     <>
-      <div className={style.box} style={{ display: "flex", marginTop: "25px " }}>
+      <div className={style.box} style={{ display: "flex", marginTop: "25px ", alignItems: "center", justifyContent: "space-around" }}>
         <div style={{ width: "400px", overflowY: "hidden" }}>
           <Katalog />
         </div>
-        <div className={style.slide_box}>
-          <div style={{margin:"0 auto",width:"100%", display:"flex", alignItems:"center", justifyContent:"center"}}>
-            <Slider />
-          </div>
-          <div>
-            {/* <ul className={style.list}>
-              <li>
-                <img src={"../../../assets/free.svg"} alt="" />
-                <h4>Бесплатная  сборка</h4>
-              </li>
-              <li>
-                <img src="../../../assets/bolib.svg" alt="" />
-                <h4>Рассрочка 4 мес./0%
-                  без переплат</h4>
-              </li>
-              <li>
-                <img src="../../../assets/dastavka.svg" alt="" />
-                <h4>Бесплатная
-                  доставка</h4>
-              </li>
-              <li>
-                <img src="../../../assets/year.png" alt="" />
-                <h4>Официальная
-                  гарантия</h4>
-              </li>
-              <li>
-                <img src="../../../assets/best_money.svg" alt="" />
-                <h4>Лучшая цена
-                </h4>
-              </li>
-              <li>
-                <img src="../../../assets/year.png" alt="" />
-                <h4>11 лет
-                  на рынке</h4>
-              </li>
-              <li>
-                <img src="../../../assets/consult.svg" alt="" />
-                <h4>Профессиональная
-                  консультация</h4>
-              </li>
-            </ul> */}
-          </div>
+        <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+          <Slider />
+
+          <ul className={style.list}>
+            <li>
+              <img src="../assets/free.svg" alt="" />
+              <h2>Бесплатная
+                сборка</h2>
+            </li>
+            <li>
+              <img src="../assets/bolib.svg" alt="" />
+              <h2>Рассрочка 4 мес./0%
+                без переплат</h2>
+            </li>
+            <li>
+              <img src="../assets/dastavka.svg" alt="" />
+              <h2>Бесплатная
+                доставка</h2>
+            </li>
+            <li>
+              <img src="../assets/garant.svg" alt="" />
+              <h2>Официальная
+                гарантия</h2>
+            </li>
+            <li>
+              <img src="../assets/best_money.svg" alt="" />
+              <h2>Лучшая цена
+              </h2>
+            </li>
+            <li>
+              <img src="../assets/year.png" alt="" />
+              <h2>11 лет
+                на рынке</h2>
+            </li>
+            <li>
+              <img src="../assets/consult.svg" alt="" />
+              <h2>Профессиональная
+                консультация</h2>
+            </li>
+          </ul>
         </div>
+
       </div>
     </>
   )
