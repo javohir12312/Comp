aa3c07efa5332d2eaa00842ac832e72df1ff34d7
javohir12312 <abduxalilovjavohir393@gamil.com>
2023-05-03 17:01:18 +0500

    g

--- src/components/Header/Katalog/Katalog.jsx
+++ src/components/Header/Katalog/Katalog.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import style from "./Katalog.module.scss"
 import { MailOutlined, SettingOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
-
+import { Menu } from 'antd'; import { IconName, IoDesktop, IoHardwareChip, IoLaptopOutline } from "react-icons/io5";
+import { BsFillHddStackFill, BsLaptop } from "react-icons/bs";
+import {FaGamepad} from "react-icons/fa"
+import {AiFillPrinter, AiOutlineSound, AiOutlineTablet} from "react-icons/ai"
+import {GiWifiRouter} from "react-icons/gi"
 function getItem(label, key, icon, children, type) {
   return {
     key,
@@ -14,51 +17,45 @@ function getItem(label, key, icon, children, type) {
 }
 
 const items = [
-  getItem('Комплектующие ПК', 'sub1', <MailOutlined />, [
+  getItem('Комплектующие ПК', 'sub1', <IoHardwareChip size={25} />, [
     getItem(null, null, null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
     getItem(null, null, null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
   ]),
-  getItem('Мониторы', 'sub2', <MailOutlined />, [
+  getItem('Мониторы', 'sub2', <IoDesktop  size={25}/>, [
     getItem('Option 5', '5'),
     getItem('Option 6', '6'),
     getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
   ]),
-  getItem('Компьютеры', 'sub4', <SettingOutlined />, [
+  getItem('Компьютеры', 'sub4', <BsFillHddStackFill  size={25}/>, [
     getItem('Option 9', '9'),
     getItem('Option 10', '10'),
     getItem('Option 11', '11'),
     getItem('Option 12', '12'),
   ]),
-  getItem('Ноутбуки', 'sub5', <MailOutlined />, [
+  getItem('Ноутбуки', 'sub5', < BsLaptop size={25}/>, [
     getItem(null, null, null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
     getItem(null, null, null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
   ]),
-  getItem('Игровые консоли', 'sub6', <MailOutlined />, [
+  getItem('Игровые консоли', 'sub6', <FaGamepad size={25} />, [
     getItem('Option 5', '5'),
     getItem('Option 6', '6'),
     getItem('Submenu', 'sub7', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
   ]),
-  getItem('Комплектующие к ноутбукам', 'sub8', <SettingOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
-    getItem('Option 11', '11'),
-    getItem('Option 12', '12'),
-  ]),
-  getItem('Планшеты', 'sub9', <MailOutlined />, [
+  getItem('Планшеты', 'sub9', <AiOutlineTablet size={25} />, [
     getItem(null, null, null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
     getItem(null, null, null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
   ]),
-  getItem('Принтеры и МФУ', 'sub10', <MailOutlined />, [
+  getItem('Принтеры и МФУ', 'sub10', <AiFillPrinter size={25} />, [
     getItem('Option 5', '5'),
     getItem('Option 6', '6'),
     getItem('Submenu', 'sub11', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
   ]),
-  getItem('Акустические колонки', 'sub12', <SettingOutlined />, [
+  getItem('Акустические колонки', 'sub12', <AiOutlineSound size={25} />, [
     getItem('Option 9', '9'),
     getItem('Option 10', '10'),
     getItem('Option 11', '11'),
     getItem('Option 12', '12'),
-  ]),getItem('Комплектующие к ноутбукам', 'sub13', <SettingOutlined />, [
+  ]), getItem('Сетевое оборудование', 'sub13', <GiWifiRouter size={25} />, [
     getItem('Option 9', '9'),
     getItem('Option 10', '10'),
     getItem('Option 11', '11'),
@@ -100,8 +97,8 @@ const items = [
     getItem('Option 11', '11'),
     getItem('javava 12', '12'),
   ]),
-  
-  
+
+
 ];
 const onClick = (e) => {
   console.log('click', e);
@@ -110,7 +107,7 @@ const onClick = (e) => {
 
 const Katalog = () => (
   <Menu
-  className={style.menu}
+    className={style.menu}
     onClick={onClick}
     mode="vertical"
     items={items}
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,58 @@
 import React from 'react'
 import Slider from './Slider/Slider'
-import style from  "./Home.module.scss"
+import style from "./Home.module.scss"
 import Katalog from "../Header/Katalog/Katalog"
 
 const Home = () => {
   return (
     <>
-      <div className={style.box} style={{display:"flex",marginTop:"25px "} }>
-        <div style={{width:"400px", overflowY:"hidden"}}>
-          <Katalog/>
+      <div className={style.box} style={{ display: "flex", marginTop: "25px " }}>
+        <div style={{ width: "400px", overflowY: "hidden" }}>
+          <Katalog />
         </div>
-        <Slider />
+        <div className={style.slide_box}>
+          <div style={{margin:"0 auto",width:"100%", display:"flex", alignItems:"center", justifyContent:"center"}}>
+            <Slider />
+          </div>
+          <div>
+            {/* <ul className={style.list}>
+              <li>
+                <img src={"../../../assets/free.svg"} alt="" />
+                <h4>Бесплатная  сборка</h4>
+              </li>
+              <li>
+                <img src="../../../assets/bolib.svg" alt="" />
+                <h4>Рассрочка 4 мес./0%
+                  без переплат</h4>
+              </li>
+              <li>
+                <img src="../../../assets/dastavka.svg" alt="" />
+                <h4>Бесплатная
+                  доставка</h4>
+              </li>
+              <li>
+                <img src="../../../assets/year.png" alt="" />
+                <h4>Официальная
+                  гарантия</h4>
+              </li>
+              <li>
+                <img src="../../../assets/best_money.svg" alt="" />
+                <h4>Лучшая цена
+                </h4>
+              </li>
+              <li>
+                <img src="../../../assets/year.png" alt="" />
+                <h4>11 лет
+                  на рынке</h4>
+              </li>
+              <li>
+                <img src="../../../assets/consult.svg" alt="" />
+                <h4>Профессиональная
+                  консультация</h4>
+              </li>
+            </ul> */}
+          </div>
+        </div>
       </div>
     </>
   )
